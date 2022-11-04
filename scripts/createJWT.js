9b3d25322de2e1714bafd24ed00966fe14af369d
Edison Pizarro Godoy <edipizarro@uc.cl>
2022-11-04 04:55:27 -0300

    refactor/easier-use Admin level on script example

--- scripts/createJWT.js
+++ scripts/createJWT.js
@@ -1,5 +1,5 @@
-var jwt = require('jsonwebtoken');
-var dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 dotenv.config();
 
 const currentTimeEpoch = Math.floor(Date.now() / 1000)
@@ -10,13 +10,13 @@ const payload = {
   aud: process.env.AUDIENCE, // aud must be the same as the one in the chat api
   iss: process.env.ISSUER, // iss must be the same as the one in the chat api
   exp: expiryTimeEpoch, // Epoch time must be later than now
-  sub: "123546789",
+  sub: "88c1d984-69bd-4926-906b-7023c070b434",
   entityUUID: "88c1d984-69bd-4926-906b-7023c070b434",
-  userUUID: "eff2697a-8e9d-40ce-a461-f3fbc5739ace",
-  levelOnEntity: 100
+  userUUID: "88c1d984-69bd-4926-906b-7023c070b434",
+  levelOnEntity: 999
 }
 
-var token = jwt.sign(
+const token = jwt.sign(
   payload,
   process.env.JWT_MASTER_SECRET
 );
